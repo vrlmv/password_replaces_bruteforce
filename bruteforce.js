83375032169656860f8a47a24f79e6e6dd160e33
Pavel <52155914+vrlmv@users.noreply.github.com>
2020-09-26 23:45:57 +0600

    Update method

--- bruteforce.js
+++ bruteforce.js
@@ -1,15 +1,16 @@
-
-
-String.prototype.replaceAt=function(index, replacement) {
-    return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
+String.prototype.replaceAt = function (index, replacement) {
+    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
 }
 
-class Passwords
-{
+var BreakException = {};
+
+class Passwords {
 
     words;
     replaces;
     hash;
+    brutedPassword = {};
+    currWord = '';
 
     constructor(words, replaces, hash) {
         this.words = words;
@@ -19,23 +20,37 @@ class Passwords
 
     findPassword() {
         let passwords = [];
-        let brutedPassword = '';
 
-        this.words.forEach(word => {
-            if (MD5(word) === this.hash) {
-                brutedPassword = word;
-            }
-            passwords = this.findReplaces(word);
-
-            passwords.forEach(password => {
-                let passwordHash = MD5(password);
-                if (passwordHash === this.hash) {
-                    brutedPassword = password;
+        try {
+            this.words.forEach(word => {
+                this.currWord = word;
+                if (JSON.stringify(this.brutedPassword) !== JSON.stringify({})) {
+                    return;
                 }
-            })
-        });
 
-        return brutedPassword;
+                if (MD5(word.trim()) === this.hash) {
+                    (this.brutedPassword)['originalWord'] = word;
+                    (this.brutedPassword)['startWord'] = word;
+                    throw BreakException;
+                }
+                let passwordCombinations = this.findReplaces(word);
+
+                // passwordCombinations.forEach(combination => {
+                //
+                //     if (MD5(combination) === this.hash) {
+                //         (this.brutedPassword)['originalWord'] = combination;
+                //         (this.brutedPassword)['startWord'] = word;
+                //         throw BreakException;
+                //     }
+                // })
+
+            });
+        } catch (e) {
+            if (e !== BreakException) throw e;
+        }
+
+
+        return this.brutedPassword;
     }
 
     findReplaces(word) {
@@ -43,38 +58,68 @@ class Passwords
         let strArr = [];
         let result = [];
 
-        for (let symbol in this.replaces) {
-            if (symbol === '') {
-                continue;
-            }
-            positions = Object.assign(this.strpos_recursive(word, symbol), positions);
-        }
 
+        for (let index in this.replaces) {
+            let symbols = this.replaces[index];
+            for (let symbol in symbols) {
+                if (symbol === '') {
+                    continue;
+                }
+
+                positions = Object.assign(this.strpos_recursive(word, index), positions);
+            }
+        }
         let combinations = this.getCombinations(Object.keys(positions));
         let str = word;
-
         combinations.forEach(combination => {
             let variant = combination.split('_');
 
             strArr = str.split('');
-
             variant.forEach(offset => {
+
                 strArr[offset] = positions[offset];
             })
-
-            result.push(strArr.join(''));
+            result.push(strArr);
         });
 
-        return result;
+        result.forEach(vart => {
+            this.getHardCombinations(vart);
+        })
 
 
     }
 
+    getHardCombinations(variant) {
+        let combos = [],
+            indexes = [];
 
+        variant.forEach((char, index) => {
+            if (Array.isArray(char)) {
+                indexes.push(index);
+                combos.push(char);
+            }
+        });
+
+        let combine = combineArrays(combos);
+
+        combine.forEach((combo, index) => {
+            let passwd = variant;
+            let positions = combo.split('_').join('').split(''); // [0] ==== [0,2,// 1,3]
+            positions.forEach((positionInVariant, indexOfString) => {
+                let symbol = combos[indexOfString][positionInVariant]; // y
+                passwd[indexes[indexOfString]] = symbol;
+            })
+            if (MD5(passwd.join('').trim()) === this.hash) {
+                (this.brutedPassword)['originalWord'] = passwd.join('');
+                (this.brutedPassword)['startWord'] = this.currWord;
+                throw BreakException;
+            }
+        })
+    }
 
     getCombinations(chars) {
         var result = [];
-        var f = function(prefix, chars) {
+        var f = function (prefix, chars) {
             for (var i = 0; i < chars.length; i++) {
                 result.push(prefix + chars[i]);
                 f(prefix + chars[i] + '_', chars.slice(i + 1));
@@ -89,14 +134,66 @@ class Passwords
         if (offset1 === false) {
             return results;
         } else {
+
             results[offset1] = this.replaces[needle];
-            return this.strpos_recursive(haystack, needle, offset1+1, results);
+            return this.strpos_recursive(haystack, needle, offset1 + 1, results);
         }
     }
 
-    strpos (haystack, needle, offset) {
+    strpos(haystack, needle, offset) {
         var i = (haystack + '')
             .indexOf(needle, (offset || 0))
         return i === -1 ? false : i
+    }
+}
+
+function combineArrays(array_of_arrays) {
+    if (!array_of_arrays) {
+        return [];
+    }
+    if (!Array.isArray(array_of_arrays)) {
+        return [];
+    }
+    if (array_of_arrays.length == 0) {
+        return [];
+    }
+    for (let i = 0; i < array_of_arrays.length; i++) {
+        if (!Array.isArray(array_of_arrays[i]) || array_of_arrays[i].length == 0) {
+            return [];
+        }
+    }
+    let odometer = new Array(array_of_arrays.length);
+    odometer.fill(0);
+    let output = [];
+    let newCombination = formCombination(odometer, array_of_arrays);
+    output.push(newCombination);
+    while (odometer_increment(odometer, array_of_arrays)) {
+        newCombination = formCombination(odometer, array_of_arrays);
+        output.push(newCombination);
+    }
+    return output;
+}
+
+
+function formCombination(odometer, array_of_arrays) {
+    return odometer.reduce(function (accumulator, odometer_value, odometer_index) {
+        return "" + accumulator + odometer_value + "_";
+    }, "");
+}
+
+function odometer_increment(odometer, array_of_arrays) {
+    for (let i_odometer_digit = odometer.length - 1; i_odometer_digit >= 0; i_odometer_digit--) {
+        let maxee = array_of_arrays[i_odometer_digit].length - 1;
+        if (odometer[i_odometer_digit] + 1 <= maxee) {
+            odometer[i_odometer_digit]++;
+            return true;
+        } else {
+            if (i_odometer_digit - 1 < 0) {
+                return false;
+            } else {
+                odometer[i_odometer_digit] = 0;
+                continue;
+            }
+        }
     }
 }
